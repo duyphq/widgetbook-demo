75ff246d7486fb3c88159eb5221e72f6996de0f2
duypham <duyhqpham@gmail.com>
2024-06-10 14:17:14 +0700

    feat: update builder contain

--- lib/widgetbook.dart
+++ lib/widgetbook.dart
@@ -13,46 +13,61 @@ class WidgetbookApp extends StatelessWidget {
   const WidgetbookApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return Widgetbook.material(directories: directories, addons: [
-      ThemeAddon(
-        themes: [
-          WidgetbookTheme(
-            name: 'Light',
-            data: lightTheme,
-          ),
-          WidgetbookTheme(
-            name: 'Dark',
-            data: darkTheme,
-          ),
-        ],
-        themeBuilder: (context, theme, child) => ColoredBox(
-          color: theme.surface.primary,
-          child: DefaultTextStyle(
-            style: theme.typography.bodyMedium16,
-            child: AppTheme(
-              data: theme,
+    return Widgetbook.material(
+        directories: directories,
+        appBuilder: (context, child) => ColoredBox(
+              color: Colors.white,
               child: child,
             ),
-          ),
-        ),
-      ),
-      DeviceFrameAddon(
-        devices: [
-          Devices.ios.iPhoneSE,
-          Devices.ios.iPhone13,
-          Devices.android.samsungGalaxyS20
+        integrations: [
+          WidgetbookCloudIntegration(),
         ],
-        initialDevice: Devices.ios.iPhone13,
-      ),
-      TextScaleAddon(
-        scales: [1.0, 2.0],
-      ),
-      LocalizationAddon(locales: [
-        const Locale('en', 'US'),
-      ], localizationsDelegates: [
-        DefaultWidgetsLocalizations.delegate,
-        DefaultMaterialLocalizations.delegate,
-      ], initialLocale: const Locale('en', 'US')),
-    ]);
+        addons: [
+          DeviceFrameAddon(
+            devices: [
+              Devices.ios.iPhoneSE,
+              Devices.ios.iPhone13,
+              Devices.android.samsungGalaxyS20
+            ],
+            initialDevice: Devices.ios.iPhone13,
+          ),
+          TextScaleAddon(
+            scales: [1.0, 2.0],
+          ),
+          ThemeAddon(
+            themes: [
+              WidgetbookTheme(
+                name: 'Light',
+                data: lightTheme,
+              ),
+              WidgetbookTheme(
+                name: 'Dark',
+                data: darkTheme,
+              ),
+            ],
+            themeBuilder: (context, theme, child) => ColoredBox(
+              color: theme.surface.primary,
+              child: DefaultTextStyle(
+                style: theme.typography.bodyMedium16,
+                child: AppTheme(
+                  data: theme,
+                  child: child,
+                ),
+              ),
+            ),
+          ),
+          LocalizationAddon(locales: [
+            const Locale('en', 'US'),
+          ], localizationsDelegates: [
+            DefaultWidgetsLocalizations.delegate,
+            DefaultMaterialLocalizations.delegate,
+          ], initialLocale: const Locale('en', 'US')),
+          BuilderAddon(
+            name: 'SafeArea',
+            builder: (_, child) => SafeArea(
+              child: child,
+            ),
+          )
+        ]);
   }
 }
